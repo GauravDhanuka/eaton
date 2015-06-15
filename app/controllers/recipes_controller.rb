d0d58f9d8531f47a67565698d33904dd442c004f
GauravDhanuka <gauravdhanuka2007@gmail.com>
2015-06-15 11:58:17 +0530

    Added Users and Authentication

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,8 @@ class RecipesController < ApplicationController
 
 	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
+	before_action :authenticate_user!, except: [:index, :show]
+
 	def index
 		@recipes = Recipe.all.order("created_at DESC")
 	end
@@ -11,11 +13,11 @@ class RecipesController < ApplicationController
 	end
 
 	def new
-		@recipe = Recipe.new
+		@recipe = current_user.recipes.build
 	end
 
 	def create
-		@recipe = Recipe.new(recipe_params)
+		@recipe = current_user.recipes.build(recipe_params)
 		
 		if @recipe.save
 			redirect_to @recipe, notice: "Successfully created new recipe"
