a0cc9e0538cc5c3bb5b206d151ae50a5a777abc4
GauravDhanuka <gauravdhanuka2007@gmail.com>
2015-06-16 12:12:31 +0530

    modifications in Edit Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -14,7 +14,7 @@ class Recipe < ActiveRecord::Base
 
 	validates :title, :description, :image, presence: true
 
-	has_attached_file :image, styles: { medium: "400x400#" }
+	has_attached_file :image, styles: { medium: "400x400#", small: "100x100#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
